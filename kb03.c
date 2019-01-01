159c64c5068f2a33668351c355f5d32f6adffd99
boomak <46297167+boomak@users.noreply.github.com>
2019-01-01 06:34:18 -0800

    Update kb03.c

--- kb03.c
+++ kb03.c
@@ -3,14 +3,20 @@
 int main(void) {
 char n;
 scanf("%c",&n);
+if(n>='a'&&n<='z'||n>='A'&&n<='Z')
+{
 if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
 {
 	printf("vowel");
 }
+else 
+{
+	printf("Consonant");
+}
+}
 else
 {
 	printf("Invalid");
-
 }
 return 0;
 }
