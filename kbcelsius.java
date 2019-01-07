6a9e341b0d535d5039df8e1c77c30515ed7757b7
boomak <46297167+boomak@users.noreply.github.com>
2019-01-07 07:54:28 -0800

    Create kbcelsius.java

---
+++ kbcelsius.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class celsius {
+
+	public static void main(String[] args)
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int  kelvin;
+	  int celsius=sc.nextInt();
+	  kelvin=celsius+273;
+	  System.out.println(kelvin);
+	  
+	}
+}
+	  
+	 
+
+
