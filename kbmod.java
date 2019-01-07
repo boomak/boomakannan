5d74cdf5af393ad829234a96c14d2acce0e866e1
boomak <46297167+boomak@users.noreply.github.com>
2019-01-07 08:44:25 -0800

    Create kbmod.java

---
+++ kbmod.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+class kbmod {
+
+	public static void main(String[] args)
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int a=sc.nextInt();
+	  int b=sc.nextInt();
+	  int c=sc.nextInt();
+	  int d=(a*b)%c;
+	  System.out.println(d);
+	  
+	}
+}
+	  
+	 
+
+
