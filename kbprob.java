1a980a1ba2f486f7b5b4eb233c1d3e2651e37a11
boomak <46297167+boomak@users.noreply.github.com>
2019-01-07 07:22:43 -0800

    Create kbprob.java

---
+++ kbprob.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class kbprob {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int g=(a*b)/c;
+		System.out.println(g);
+		
+
+	}
+
+}
