7f001f08225e834576aaf182dac668aba3bf9b61
boomak <46297167+boomak@users.noreply.github.com>
2022-05-01 18:12:43 +0530

    Challenge-4balls

---
+++ Challenge.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+import java.applet.Applet;
+
+public class Challenge extends PApplet {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    int x = 0;
+
+    public static void main(String[] args) {
+        PApplet.main("Challenge", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT); //sketch size
+    }
+
+
+    public void draw() {
+        for (int i = 1; i <= 4; i++) {
+            getEllipse(i * x, i * HEIGHT / 5);
+        }
+        x += 1;
+    }
+
+    private void getEllipse(int posX, int height) {
+        ellipse(posX, height, DIAMETER, DIAMETER);
+
+    }
+}
